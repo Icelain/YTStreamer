a768cf9d3454b30a90c993f25f68ef1bf6a436a7
Icelain <xerneas965@gmail.com>
2020-11-08 17:35:29 +0530

    Change in main player logic, help arguments, previous and restart player commands.
    Switched link-to-player middleware from a for loop to while loop for extensive control. This allowed me add the previous and restart player commands. Also added help argument to view all commands. *Re-evaluation of code is advised for anyone who wants to contribute.

--- streamer.py
+++ streamer.py
@@ -8,6 +8,7 @@ import sys,os
 import multiprocessing
 import argparse
 __version__="1.1.0"
+
 glurl=""
 
 FREE_API_KEY="AIzaSyA_qsbJMvLaklHfbLKMq4zaoVE-7UTTqcM"
@@ -20,8 +21,10 @@ def play():
 
 def checkForGi():
     if sys.platform=="linux":
+       
         try:
             from gi import require_version
+        
         except:
             print(colorama.Fore.RED+"Please install gstreamer bindings for python on linux to run ytstreamer"+colorama.Style.RESET_ALL)
             quit()
@@ -52,6 +55,7 @@ def del0(lst):
 
 def timeToInt(tstr):
     components=tstr.split(":")
+
     hourSubcomponents=list(components[0])
     minSubcomponents=list(components[1])
     secComponents=list(components[2])
@@ -59,30 +63,41 @@ def timeToInt(tstr):
     hourSubcomponents=3600 * int(listToString(del0(hourSubcomponents)))
     minSubcomponents=60 * int(listToString(del0(minSubcomponents)))
     secComponents=int(listToString(del0(secComponents)))
+
     return hourSubcomponents+minSubcomponents+secComponents+2
 
 
 
 def runmain():
     global glurl
+    
     colorama.init()
+
     checkForGi()
     cs_fig=Figlet(font="jazmine")
+
     print(colorama.Fore.LIGHTGREEN_EX,cs_fig.renderText("Yt_Streamer"),colorama.Style.RESET_ALL)
+    
     try:
         inp=input(colorama.Fore.CYAN+"Enter video query: "+colorama.Fore.YELLOW)
+    
     except KeyboardInterrupt:
         print(colorama.Fore.RED+"Exited"+colorama.Style.RESET_ALL)
         quit()
 
     res=getResults(inp)
     res_counter=1
-    for re in res:
-        video=pafy.new(re)
+
+    while res_counter < len(res):
+        video=pafy.new(res[res_counter-1])
+
         bestaudio=video.getbestaudio()
         url=bestaudio.url
+        
         glurl=url
+        
         #xz=timeToInt(video.duration) not being used currently but is required for later versions where I'll be using a custom gst player instead of playsound
+        
         print(colorama.Fore.GREEN,f"Now playing[{res_counter}]: [{video.title}][{video.duration}][@{bestaudio.quality}]",colorama.Style.RESET_ALL)
 
 
@@ -90,6 +105,7 @@ def runmain():
         process.start()
         try:
             args=input("~ ")
+
         except KeyboardInterrupt:
             process.terminate()
             print(colorama.Fore.RED+"\nStream Stopped"+colorama.Style.RESET_ALL)
@@ -105,17 +121,35 @@ def runmain():
             process.terminate()
             res_counter+=1
             continue
+        if args.lower()=="restart" or args.lower()=="r":
+        	process.terminate()
+        	res_counter=1
+        	continue    
+        if args.lower=="previous" or args.lower()=="p":
+        	process.terminate()
+        	res_counter-=1
+        	continue	
 
     print(colorama.Fore.RED+"\nQueue finished")
     print("Exited")
 
 def main():
     parser=argparse.ArgumentParser()
+
     parser.add_argument("-v","--version",dest="ver",action="store_true")
+    parser.add_argument("-hy","--yelp",dest="hbool",action="store_true")
+    
     recv_args=parser.parse_args()
+
     if recv_args.ver:
         print(__version__)
+
+    elif recv_args.hbool:
+    	print("Help:\n\n-v or --version- Displays version\n-h or --help - Displays Help\n\nPlayer commands-\n\n1. n or next to get to the next stream\n2. q or quit to quit the script.(Ctrl-C also works)\n3. r or restart to restart the queue\n4. p or previous to go back to previous stream. ")    
+    
     else:
         runmain()
+
 '''if __name__=="__main__":
-    runmain()'''
+    main()'''
+
